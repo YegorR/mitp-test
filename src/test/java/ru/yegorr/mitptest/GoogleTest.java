31887afddf46e22b8407dd7aa927a0fcfb43af2a
Egor Ryazantsev <egorryazancev1908@mail.ru>
2020-12-07 11:35:02 +0700

    yes

--- src/test/java/ru/yegorr/mitptest/GoogleTest.java
+++ src/test/java/ru/yegorr/mitptest/GoogleTest.java
@@ -1,11 +1,34 @@
 package ru.yegorr.mitptest;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class GoogleTest {
 
+    private final static String URL = "https://yandex.ru";
+
+    private static MainPage mainPage;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.gecko.driver",
+                GoogleTest.class.getClassLoader().getResource("geckodriver.exe").getPath());
+
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(URL);
+
+        mainPage = new MainPage(driver);
+    }
+
     @Test
     public void test() {
-
+        mainPage.printQuery("Хочу персиков");
     }
 }
--- src/test/java/ru/yegorr/mitptest/MainPage.java
+++ src/test/java/ru/yegorr/mitptest/MainPage.java
@@ -3,16 +3,18 @@ package ru.yegorr.mitptest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class MainPage {
 
     private WebDriver webDriver;
 
     public MainPage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver,this);
         this.webDriver = webDriver;
     }
 
-    @FindBy(id = "text")
+    @FindBy(xpath = ".//input[@id='text']")
     private WebElement queryLine;
 
     public void printQuery(String text) {
